c581c45125e0a56e8c87e401b83fec79fe54ba04
Daniel Ivanov <jenasoplo69@gmail.com>
2025-11-24 18:27:08 +0200

    Daniel Ivanov, lab 8

---
+++ app/src/main/java/ua/opnu/Printer.java
@@ -0,0 +1,10 @@
+package ua.opnu;
+
+public class Printer {
+    public <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
